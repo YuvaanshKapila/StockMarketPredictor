597728c49652cb250e334ec38f06cf837c20715b
Yuvaansh Kapila <140757236+YuvaanshKapila@users.noreply.github.com>
2025-07-30 13:38:54 -0400

    change port

--- backend/app.py
+++ backend/app.py
@@ -93,4 +93,5 @@ if __name__ == '__main__':
     physical_devices = tf.config.list_physical_devices('GPU')
     for device in physical_devices:
         tf.config.experimental.set_memory_growth(device, True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
