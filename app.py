6ff8f6843f683aca1a331a8a3cdfaaec7c0c0bd5
Yuvaansh Kapila <140757236+YuvaanshKapila@users.noreply.github.com>
2025-08-05 02:39:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -88,10 +88,9 @@ def predict():
         'test_actual': actual.flatten().tolist(),
         'test_predicted': predicted.flatten().tolist()
     })
-
 if __name__ == '__main__':
     physical_devices = tf.config.list_physical_devices('GPU')
     for device in physical_devices:
         tf.config.experimental.set_memory_growth(device, True)
 
-    app.run()
+    app.run(host='0.0.0.0', port=40949)
